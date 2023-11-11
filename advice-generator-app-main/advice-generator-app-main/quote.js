465991103ab9d95b879c4d4e926781847aa1d73f
unknown <kevinpena421@gmail.com>
2023-11-10 20:01:15 -0600

    got api working

--- advice-generator-app-main/advice-generator-app-main/quote.js
+++ advice-generator-app-main/advice-generator-app-main/quote.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+    fetch('https://api.adviceslip.com/advice')
+        .then(res => res.json())
+        .then(data => {
+            document.querySelector('.quote').innerHTML = data.slip.advice
+            document.querySelector('#advice-number').innerHTML += data.slip.id})
+
+        .catch(err => console.log(err))
+        
+
