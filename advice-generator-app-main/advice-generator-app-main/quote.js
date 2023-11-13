9ddee7809a08a34b86874b490ff9d8e1dbf56afe
kevin pena <kevinpena421@gmail.com>
2023-11-12 22:01:52 -0600

    added api and fixed errors

--- advice-generator-app-main/advice-generator-app-main/quote.js
+++ advice-generator-app-main/advice-generator-app-main/quote.js
@@ -1,1 +1,29 @@
-console.log('hello world');
+// function VideoGameFactory(name,genre,console) {
+//     this.name = name
+//     this.genre = genre
+//     this.console = console
+
+//     this.nameFunc = function () {
+//         alert(`this is my game ${this.name}`);
+//     }
+
+//     this.genreFunc = function () {
+//         alert(`this is my genre ${this.genre}`);
+//     }
+
+//     this.consoleFunc = function () {
+//         alert(`this is my console ${this.console}`);
+//     }
+// }
+
+// let fallout3 = new VideoGameFactory('fallout3','rpg','pc')
+
+// const granTurismo = new VideoGameFactory('granturismo','racing','playstation');
+// data.slip
+fetch('https://api.adviceslip.com/advice')
+
+    .then(res => res.json())
+    .then(data => {console.log(data.slip.advice)
+    document.querySelector('.quote').innerHTML = data.slip.advice
+    document.querySelector('#advice').innerHTML += data.slip.id})
+    .catch(err => console.log(err))
