d03087a8c0992f58727995ca366413160f930450
kevin pena <kevinpena421@gmail.com>
2023-11-12 22:12:15 -0600

    fixed errors with id

--- advice-generator-app-main/advice-generator-app-main/quote.js
+++ advice-generator-app-main/advice-generator-app-main/quote.js
@@ -1,6 +1,6 @@
     fetch('https://api.adviceslip.com/advice')
         .then(res => res.json())
-        .then(data => {
+        .then(data => { console.log(data.slip.advice);
             document.querySelector('.quote').innerHTML = data.slip.advice
             document.querySelector('#advice-number').innerHTML += data.slip.id})
 
